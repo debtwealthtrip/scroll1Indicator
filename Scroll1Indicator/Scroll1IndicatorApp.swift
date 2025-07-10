e823f6964f584ded3f095b1ce734db408bbde358
debtwealthtrip <debtwealthtrip@gmail.com>
2025-07-10 13:33:54 -0400

    add indicator
    added

---
+++ Scroll1Indicator/Scroll1IndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll1IndicatorApp.swift
+//  Scroll1Indicator
+//
+//  Created by sam win on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Scroll1IndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
